0e3ef3d2242e1984aeada27825f34ecd3b48d1a9
doodlepuff <>
2022-03-21 19:53:33 +0000

    Added IDs, version before search by layer

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from pydictionary import Dictionary
+import time
 
 class Node:
-  def __init__(self, value, parent=None):
+  def __init__(self, value, parent=None,id=0):
     self.value = value
     self.parent = parent
     self.children = []
     self.root = None
     self.maxDepth = 0
+    self.totalChildren = 0
+    self.id = 0
     if self.parent != None:
       p = self.parent
       while p.parent != None:
@@ -16,8 +19,18 @@ class Node:
   def addChild(self,value):
     child = Node(value,parent=self)
     self.children.append(child)
+    if self.root != None:
+      self.root.totalChildren += 1
+      child.id = self.root.totalChildren
+      if self.root.totalChildren % 10 == 0:
+        print("{} children of root.".format(self.root.totalChildren))
+    else:
+      self.totalChildren += 1
+      child.id = self.totalChildren
+      if self.totalChildren % 10 == 0:
+        print("{} children of root.".format(self.totalChildren))
     return child
-
+  
   def getSynonyms(self):
     for word in Dictionary(self.value,100).synonyms():
       if " " in word:
@@ -26,6 +39,15 @@ class Node:
         self.addChild(word)
       elif self.root.value != word and self.root.searchNode(self.root,word) == None:
         self.addChild(word)
+
+  def getAllNodes(self):
+    if len(self.children) == 0:
+      return [self]
+    nodeList = []
+    for c in self.children:
+      for n in c.getAllNodes():
+        nodeList.append(n)
+    return nodeList
     
   def searchNode(self, node, value):
     if node.value == value:
@@ -40,27 +62,35 @@ class Node:
         nodeFound = searchVal
     return nodeFound
 
-  def expandSynonyms(self, node, level):
-    print("{}\t{}".format(level,node.value))
+  def expandSynonyms(self, node, level, limit):
+    if level == limit:
+      return
     if node.root != None and level > node.root.maxDepth:
       node.root.maxDepth = level
-      print("Max depth: {}".format(level))
-    numSynonyms = node.getSynonyms()
+      #print("Max depth: {}".format(level))
+    node.getSynonyms()
     if len(node.children) == 0:
       return
     for n in node.children:
-      self.expandSynonyms(n, level+1)
+      self.expandSynonyms(n, level+1,limit)
+
+  def saveNodeToText(self):
+    #id,value,children
+    retStr = "id={},val={},children=".format(self.id,self.value)
+    for c in self.children:
+      retStr+= c.id
+    return retStr
     
   def __str__(self):
     retStr = "--- {}\n".format(self.value.upper())
     index = 0
     for n in self.children:
-      retStr+="{}. {}, ".format(index, n.value)
+      retStr+="{}. {}, """.format(index, n.value)
       index += 1
     return retStr
 
-ROOT_WORD = "homonym"
-
+ROOT_WORD = "aluminum"
+DEPTH_LIMIT = 3
 def clearScreen():
   print("\033[H\033[J", end="")
 
@@ -72,7 +102,7 @@ def printMenu(node,depth):
     maxDepth = node.maxDepth
   else:
     maxDepth = node.root.maxDepth
-  print("Root: {}\t\tCurrent depth: {}/{}\t\tCurrent width: {}\n\n".format(ROOT_WORD.upper(),depth,maxDepth,width))
+  print("Root: {}\t\tCurrent depth: {}/{}\t\tCurrent width: {}\t\t{:.2f} seconds elapsed.\n\n".format(ROOT_WORD.upper(),depth,maxDepth,width,elapsedTime))
   print("{}\n\n".format(node))
   try:
     choice = int(input("Choice: ").strip())
@@ -91,14 +121,21 @@ def printMenu(node,depth):
     print("Error")
   input()
   printMenu(node,depth)
-  
+
+startTime = time.time()
 
 print("Searching...") 
 root = Node(ROOT_WORD)
-root.expandSynonyms(root,0)
-printMenu(root,0)
+root.expandSynonyms(root,0,DEPTH_LIMIT)
 
+elapsedTime = time.time()-startTime
+nodeList = root.getAllNodes()
 
+for n in nodeList:
+  print(n.id,n.value)
+
+printMenu(root)
+# id, value, children
 
 
 
